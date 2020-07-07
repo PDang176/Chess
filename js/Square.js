63d54288aa7fc2cf5ad798e84107e4c319dce20a
Patrick Dang <43787923+PDang176@users.noreply.github.com>
2020-07-07 03:54:45 -0700

    Fixed pawn issue on checks

--- js/Square.js
+++ js/Square.js
@@ -6,14 +6,26 @@ class Square{
         this.y = null;
         this.index = null;
     }
-    
+
     inCheck(board, currentTurn){
         for(let i = 0; i < board.length; i++){
             if(board[i].piece.color !== currentTurn && board[i].piece.color !== ''){
-                let positions = board[i].piece.possibleMoves(board, board[i])
-                for(let j = 0; j < positions.length; j++){
-                    if(positions[j] === this.index){
+                if(board[i].piece.symbol === '&#9817'){
+                    if(this.index === i - 7 || this.index === i - 9){
                         return true;
+                    }
+                }
+                else if(board[i].piece.symbol === '&#9823'){
+                    if(this.index === i + 7 || this.index === i + 9){
+                        return true;
+                    }
+                }
+                else{
+                    let positions = board[i].piece.possibleMoves(board, board[i])
+                    for(let j = 0; j < positions.length; j++){
+                        if(positions[j] === this.index){
+                            return true;
+                        }
                     }
                 }
             }
